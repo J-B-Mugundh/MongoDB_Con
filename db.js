ac257c1acc71f42b93c33f5cd03327d40d7b8480
Mugundh <mugundhjb@gmail.com>
2023-09-30 23:52:36 +0530

    Connecting with MongoDB Atlas

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
+// Mongo Atlas
+let uri = 'mongodb+srv://mugundhjb:mass_123@cluster0.pfmciqd.mongodb.net/?retryWrites=true&w=majority'
 
 module.exports = {
     connectToDb : (cb) => { // cb => callback function that runs after the connection is established
-        MongoClient.connect('mongodb://127.0.0.1:27017/bookstore')
+        // MongoClient.connect('mongodb://127.0.0.1:27017/bookstore') // localhost
+        MongoClient.connect(uri)
         .then((client) => {
             dbConnection = client.db()
             return cb()
